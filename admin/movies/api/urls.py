a41badd90555a276b3aefcf5979933f5779be6b1
sbkubric <sbkubric@gmail.com>
2023-09-11 04:09:31 +0300

    feat: Extracted sqlite etl to another repo

--- admin/movies/api/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import include, path
-
-from .v1 import urls
-
-urlpatterns = [path("v1/", include(urls))]
--- admin/movies/api/v1/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import MovieApiView, MoviesListApiView
-
-urlpatterns = [
-    path("movies/", MoviesListApiView.as_view(), name="movies"),
-    path("movies/<uuid:pk>/", MovieApiView.as_view(), name="movie-detail"),
-]
